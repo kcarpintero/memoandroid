c414b6aaad9d8a59f20f0a739efb61f027b2a878
qinyusuain <qinyusuain@273b62fe-d343-0410-b236-f33c52d30e09>
2008-01-16 10:06:11 +0000

    初始导入。

---
+++ Memo/src/com/esmertec/MemoList.java
@@ -0,0 +1,13 @@
+package com.esmertec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MemoList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.edit_layout);
+    }
+}

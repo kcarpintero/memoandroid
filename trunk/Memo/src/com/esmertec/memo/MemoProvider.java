228a324764e08b244af25ac6e8a6c30f75254c10
qinyusuain <qinyusuain@273b62fe-d343-0410-b236-f33c52d30e09>
2008-01-17 10:36:21 +0000

    Change to ComtentProvider

--- trunk/Memo/src/com/esmertec/memo/MemoProvider.java
+++ trunk/Memo/src/com/esmertec/memo/MemoProvider.java
@@ -1,46 +1,229 @@
 package com.esmertec.memo;
 
+import java.util.HashMap;
+
+import android.content.ContentProvider;
+import android.content.ContentProviderDatabaseHelper;
+import android.content.ContentURIParser;
 import android.content.ContentValues;
-import android.content.DatabaseContentProvider;
+import android.content.QueryBuilder;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ContentURI;
+import android.text.TextUtils;
 
-public class MemoProvider extends DatabaseContentProvider {
+import com.esmertec.provider.Memo;
 
-	public MemoProvider(String dbName, int dbVersion) {
-		super(dbName, dbVersion);
-		// TODO Auto-generated constructor stub
+public class MemoProvider extends ContentProvider {
+
+	private SQLiteDatabase mDB;
+
+	private static final String DATABASE_NAME = "memo.db";
+	private static final int DATABASE_VERSION = 2;
+
+	private static final ContentURIParser URI_MATCHER;
+
+	private static final int MEMOS = 1;
+	private static final int MEMO_ID = 2;
+
+	private static final HashMap<String, String> MEMO_LIST_PROJECTION_MAP;
+
+	private static class DatabaseHelper extends ContentProviderDatabaseHelper {
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL("CREATE TABLE memos (_id INTEGER PRIMARY KEY,"
+					+ "activity TEXT," + "description TEXT," + "contact TEXT,"
+					+ "location TEXT," + "date INTEGER," + "time INTEGER,"
+					+ "created INTEGER," + "modified INTEGER" + ");");
+
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL("DROP TABLE IF EXISTS memos");
+			onCreate(db);
+		}
+
 	}
 
-	public void bootstrapDatabase() {
+	@Override
+	public int delete(ContentURI uri, String where, String[] whereArgs) {
+		int code = URI_MATCHER.match(uri);
+		int count = 0;
+		switch (code) {
+		case MEMOS:
+			count = mDB.delete("memos", where, whereArgs);
+			break;
+		case MEMO_ID:
+			String id = uri.getPathSegment(1);
+			count = mDB.delete("memos",
+					"_id="
+							+ id
+							+ (!TextUtils.isEmpty(where) ? " AND (" + where
+									+ ')' : ""), whereArgs);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URL " + uri);
+		}
+		getContext().getContentResolver().notifyChange(uri, null);
+		return count;
+	}
+
+	@Override
+	public String getType(ContentURI uri) {
+		int code = URI_MATCHER.match(uri);
+		switch (code) {
+		case MEMOS:
+			return "vnd.android.cursor.dir/com.esmertec.memos";
+		case MEMO_ID:
+			return "vnd.android.cursor.item/com.esmertec.memos";
+		default:
+			throw new IllegalArgumentException("Unknown URL " + uri);
+		}
 
 	}
 
-	public int deleteInternal(ContentURI url, String selection,
+	@Override
+	public ContentURI insert(ContentURI uri, ContentValues initialValues) {
+		if (URI_MATCHER.match(uri) != MEMOS) {
+			throw new IllegalArgumentException("Unknown URL " + uri);
+		}
+		ContentValues values;
+		if (initialValues != null) {
+			values = new ContentValues(initialValues);
+		} else {
+			values = new ContentValues();
+		}
+
+		if (!values.containsKey(Memo.Memos.ACTIVITY)) {
+			values.put(Memo.Memos.ACTIVITY, "Something");
+		}
+
+		if (!values.containsKey(Memo.Memos.CONTACT)) {
+			values.put(Memo.Memos.CONTACT, "");
+		}
+
+		if (!values.containsKey(Memo.Memos.LOCATION)) {
+			values.put(Memo.Memos.LOCATION, "");
+		}
+
+		if (!values.containsKey(Memo.Memos.DESCRIPTION)) {
+			values.put(Memo.Memos.DESCRIPTION, "");
+		}
+
+		if (!values.containsKey(Memo.Memos.DATE)) {
+			values.put(Memo.Memos.DATE, -1);
+		}
+
+		if (!values.containsKey(Memo.Memos.TIME)) {
+			values.put(Memo.Memos.TIME, -1);
+		}
+
+		Long now = Long.valueOf(System.currentTimeMillis());
+
+		// Make sure that the fields are all set
+		if (!values.containsKey(Memo.Memos.CREATED_DATE)) {
+			values.put(Memo.Memos.CREATED_DATE, now);
+		}
+
+		if (!values.containsKey(Memo.Memos.MODIFIED_DATE)) {
+			values.put(Memo.Memos.MODIFIED_DATE, now);
+		}
+
+		long rowID = mDB.insert("memos", "activity", values);
+		if (rowID > 0) {
+			ContentURI newUri = Memo.Memos.CONTENT_URI.addId(rowID);
+			getContext().getContentResolver().notifyChange(newUri, null);
+			return newUri;
+		}
+
+		throw new SQLException("Failed to insert row into " + uri);
+	}
+
+	@Override
+	public boolean onCreate() {
+		DatabaseHelper dbHelper = new DatabaseHelper();
+		mDB = dbHelper.openDatabase(getContext(), DATABASE_NAME, null,
+				DATABASE_VERSION);
+		return (mDB == null) ? false : true;
+	}
+
+	@Override
+	public Cursor query(ContentURI uri, String[] projection, String selection,
+			String[] selectionArgs, String groupBy, String having,
+			String sortOrder) {
+		QueryBuilder qb = new QueryBuilder();
+
+        switch (URI_MATCHER.match(uri)) {
+        case MEMOS:
+            qb.setTables("memos");
+            qb.setProjectionMap(MEMO_LIST_PROJECTION_MAP);
+            break;
+
+        case MEMO_ID:
+            qb.setTables("memos");
+            qb.appendWhere("_id=" + uri.getPathSegment(1));
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URL " + uri);
+        }
+
+        // If no sort order is specified use the default
+        String orderBy;
+        if (TextUtils.isEmpty(sortOrder)) {
+            orderBy = Memo.Memos.DEFAULT_SORT_ORDER;
+        } else {
+            orderBy = sortOrder;
+        }
+
+        Cursor c = qb.query(mDB, projection, selection, selectionArgs, groupBy,
+                having, orderBy);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+	}
+
+	@Override
+	public int update(ContentURI uri, ContentValues values, String selection,
 			String[] selectionArgs) {
-		return 0;
+		int code = URI_MATCHER.match(uri);
+		int count = 0;
+		switch (code) {
+		case MEMOS:
+			count = mDB.update("memos", values, selection, selectionArgs);
+			break;
+		case MEMO_ID:
+			String id = uri.getPathSegment(1);
+			count = mDB.update("memos", values, "_id="
+					+ id
+					+ (!TextUtils.isEmpty(selection) ? " AND (" + selection
+							+ ')' : ""), selectionArgs);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URL " + uri);
+		}
+		getContext().getContentResolver().notifyChange(uri, null);
+		return count;
+
 	}
 
-	public ContentURI insertInternal(ContentURI url, ContentValues values) {
-		return null;
-	}
+	static {
+		URI_MATCHER = new ContentURIParser(ContentURIParser.NO_MATCH);
+		URI_MATCHER.addURI("com.esmertec.memo", "memos", MEMOS);
+		URI_MATCHER.addURI("com.esmertec.memo", "memos/#", MEMO_ID);
 
-	public Cursor queryInternal(ContentURI url, String[] projection,
-			String selection, String[] selectionArgs, String groupBy,
-			String having, String sortOrder) {
-		return null;
-	}
-
-	public int updateInternal(ContentURI url, ContentValues values,
-			String selection, String[] selectionArgs) {
-		return 0;
-	}
-
-	public void upgradeDatabase(int oldVersion, int newVersion) {
+		MEMO_LIST_PROJECTION_MAP = new HashMap<String, String>();
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.ACTIVITY, "activity");
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.DESCRIPTION, "description");
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.CONTACT, "contact");
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.LOCATION, "location");
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.DATE, "date");
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.TIME, "time");
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.CREATED_DATE, "created");
+		MEMO_LIST_PROJECTION_MAP.put(Memo.Memos.MODIFIED_DATE, "modified");
 
 	}
 
-	public String getType(ContentURI url) {
-		return null;
-	}
 }
--- trunk/Memo/src/com/esmertec/provider/Memo.java
+++ trunk/Memo/src/com/esmertec/provider/Memo.java
@@ -5,9 +5,35 @@ import android.provider.BaseColumns;
 
 public class Memo {
 	public static final class Memos implements BaseColumns {
+		
+		public static final String DEFAULT_SORT_ORDER = "date * 1440 + time DESC";
 
 		public static final ContentURI CONTENT_URI = ContentURI
 				.create("content://com.esmertec.memo/Memos");
+
+		public static final String ACTIVITY = "activity";
+		public static final String DESCRIPTION = "description";
+		public static final String CONTACT = "contact";
+		public static final String LOCATION = "location";
+
+		public static final String DATE = "date";
+		public static final String TIME = "time";
+
+		/**
+		 * The timestamp for when the note was created
+		 * <P>
+		 * Type: INTEGER (long)
+		 * </P>
+		 */
+		public static final String CREATED_DATE = "created";
+
+		/**
+		 * The timestamp for when the note was last modified
+		 * <P>
+		 * Type: INTEGER (long)
+		 * </P>
+		 */
+		public static final String MODIFIED_DATE = "modified";
 
 	}
 

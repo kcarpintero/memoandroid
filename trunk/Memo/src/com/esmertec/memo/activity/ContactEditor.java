32a1783edb0ff7c90f18762b0a5b2bf2783fc8c9
qinyusuain <qinyusuain@273b62fe-d343-0410-b236-f33c52d30e09>
2008-01-30 10:31:56 +0000

    ToDO: 1.Add tags table in database and update MemoProvider; 2.Add TagEditor, almost like contact editor; 3.Add TitleEditor; 4.Rename some columns in "memos.db"; 5.Add the information view in MemoEditor. And...

---
+++ trunk/Memo/src/com/esmertec/memo/activity/ContactEditor.java
@@ -0,0 +1,175 @@
+package com.esmertec.memo.activity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.ContentURI;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.Menu.Item;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.esmertec.memo.R;
+import com.esmertec.memo.provider.Memo;
+import com.esmertec.memo.provider.MemoProvider;
+import com.google.wireless.gdata.data.StringUtils;
+
+public class ContactEditor extends ListActivity {
+
+	private static final String TAG = ContactEditor.class.getName();
+
+	private ContentURI mURI;
+	
+	private boolean mHasContacts;
+
+	private AutoCompleteTextView mTextContact;
+
+	@Override
+	protected void onCreate(Bundle icicle) {
+
+		super.onCreate(icicle);
+
+		Intent intent = getIntent();
+
+		if (intent == null) {
+			finish();
+			return;
+		}
+
+		if (!intent.getAction().equals(MemoProvider.ACTION_EDIT_CONTACTS)) {
+			finish();
+			return;
+		}
+
+		mURI = intent.getData();
+
+		if (mURI == null) {
+			finish();
+			return;
+		}
+
+		setContentView(R.layout.contacts_editor);
+
+		refreshList();
+
+		Button button = (Button) findViewById(R.id.button_save_auto_complete_contact);
+
+		mTextContact = (AutoCompleteTextView) findViewById(R.id.auto_complete_contact);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				addContact(mTextContact.getText().toString());
+				refreshList();
+			}
+		});
+
+	}
+
+	private void refreshList() {
+		Cursor cursor = managedQuery(mURI, new String[] { Memo.Memos.CONTACT,
+				Memo.Memos._ID }, null, null);
+		cursor.first();
+		String allContacts = cursor.getString(0);
+
+		String[] contactList = new String[] {};
+
+		if (!StringUtils.isEmpty(allContacts)) {
+			contactList = allContacts.split(", ");
+		}
+		mHasContacts = contactList.length > 0;
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, contactList));
+	}
+
+	private void deleteContact(String str) {
+		Log.v(TAG, "Delete " + str);
+		Cursor cursor = managedQuery(mURI, new String[] { Memo.Memos.CONTACT,
+				Memo.Memos._ID }, null, null);
+		cursor.first();
+		String allContacts = cursor.getString(0);
+
+		Log.v(TAG, "old allContacts = " + allContacts);
+		String[] contactList = new String[] {};
+
+		if (!StringUtils.isEmpty(allContacts)) {
+			contactList = allContacts.split(", ");
+		}
+
+		allContacts = "";
+		for (String contact : contactList) {
+			if (contact.equals(str)) {
+				continue;
+			}
+			if (!StringUtils.isEmpty(allContacts)) {
+				allContacts += ", ";
+			}
+			allContacts += contact;
+		}
+
+		Log.v(TAG, "new allContacts = " + allContacts);
+		cursor.updateString(0, allContacts);
+		managedCommitUpdates(cursor);
+		cursor.deactivate();
+	}
+
+	private void addContact(String str) {
+		Log.v(TAG, "Add " + str);
+		Cursor cursor = managedQuery(mURI, new String[] { Memo.Memos.CONTACT,
+				Memo.Memos._ID }, null, null);
+		cursor.first();
+		String allContacts = cursor.getString(0);
+
+		String[] contactList = new String[] {};
+
+		if (!StringUtils.isEmpty(allContacts)) {
+			contactList = allContacts.split(", ");
+		}
+
+		allContacts = str;
+		for (String contact : contactList) {
+			if (contact.equals(str)) {
+				continue;
+			}
+			if (!StringUtils.isEmpty(allContacts)) {
+				allContacts += ", ";
+			}
+			allContacts += contact;
+		}
+
+		cursor.updateString(0, allContacts);
+		managedCommitUpdates(cursor);
+		cursor.deactivate();
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		deleteContact(((TextView) v).getText().toString());
+		refreshList();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(Item item) {
+		// TODO Call the selected contact 
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (mHasContacts){
+			//TODO Add the menu options to call selected contact
+		}
+		return super.onPrepareOptionsMenu(menu);
+	}
+	
+	
+
+}
--- trunk/Memo/src/com/esmertec/memo/activity/MemoList.java
+++ trunk/Memo/src/com/esmertec/memo/activity/MemoList.java
@@ -19,30 +19,31 @@ import com.esmertec.memo.adapter.MemoListAdapter;
 import com.esmertec.memo.provider.Memo;
 
 public class MemoList extends ListActivity {
-	
+
 	private static final String TAG = "memolist";
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle icicle) {
 		Log.d(TAG, "onCreate");
-//		SortedMap<String, Charset> sm = Charset.availableCharsets();
-//		for ( Charset sc : sm.values()){
-//			Log.d(TAG, sc.toString());
-//		}
-		
+		// SortedMap<String, Charset> sm = Charset.availableCharsets();
+		// for ( Charset sc : sm.values()){
+		// Log.d(TAG, sc.toString());
+		// }
+
 		super.onCreate(icicle);
 
 		Intent intent = getIntent();
 		if (intent.getData() == null) {
 			intent.setData(Memo.Memos.CONTENT_URI);
 		}
-		cursor = managedQuery(getIntent().getData(), Constants.ALL_COLUMNS_PROJECTION, null, null);
+		cursor = managedQuery(getIntent().getData(),
+				Constants.ALL_COLUMNS_PROJECTION, null, null);
 
-//		ListAdapter adapter = new SimpleCursorAdapter(this,
-//				R.layout.memo_item_layout, cursor, new String[] {
-//						Memo.Memos.ACTIVITY, Memo.Memos.TIME }, new int[] {
-//						R.id.activity_column, R.id.time_column });
+		// ListAdapter adapter = new SimpleCursorAdapter(this,
+		// R.layout.memo_item_layout, cursor, new String[] {
+		// Memo.Memos.ACTIVITY, Memo.Memos.TIME }, new int[] {
+		// R.id.activity_column, R.id.time_column });
 		ListAdapter adapter = new MemoListAdapter(this,
 				R.layout.memo_item_layout, cursor);
 		setListAdapter(adapter);
@@ -52,54 +53,63 @@ public class MemoList extends ListActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-		menu.add(Menu.FIRST, ADD_MEMO_ID, R.string.menu_add_memo);
-		
+		menu.add(Menu.FIRST, ADD_MEMO_ITEM, R.string.menu_add_memo);
 
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
+		managedCommitUpdates(cursor);
 		if (cursor.count() > 0) {
-			ContentURI uri = getIntent().getData().addId(cursor.getLong(Constants.ALL_COLUMN_ID));
-			Intent[] specifics = new Intent[] { new Intent(Intent.EDIT_ACTION, uri) };
-			
+			ContentURI uri = getIntent().getData().addId(
+					cursor.getLong(Constants.ALL_COLUMN_ID));
+			Intent[] specifics = new Intent[] { new Intent(Intent.EDIT_ACTION,
+					uri) };
+
 			Intent intent = new Intent(null, uri);
 			intent.addCategory(Intent.SELECTED_ALTERNATIVE_CATEGORY);
-			
+
 			menu.addIntentOptions(Menu.SELECTED_ALTERNATIVE, 0, null,
 					specifics, intent, Menu.NO_SEPARATOR_AFTER, null);
+			if (menu.findItem(DELETE_MEMO_ITEM) == null)
+				menu.add(Menu.FIRST, DELETE_MEMO_ITEM,
+						R.string.menu_delete_memo);
 		}
 		return super.onPrepareOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(Item item) {
-		if (item.getId() == ADD_MEMO_ID) {
+		switch (item.getId()) {
+		case ADD_MEMO_ITEM:
 			Intent intent = new Intent(Intent.INSERT_ACTION, getIntent()
 					.getData());
 			startActivity(intent);
+			break;
+		case DELETE_MEMO_ITEM:
+			cursor.deleteRow();
+			managedCommitUpdates(cursor);
+			break;
 		}
+
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
 
 	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {		
+	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 		ContentURI uri = getIntent().getData().addId(cursor.getLong(0));
 		Intent intent = new Intent(Intent.EDIT_ACTION, uri);
-		Log.v("qinyu","has default category:" + intent.hasCategory(Intent.DEFAULT_CATEGORY));
+		// Log.v("qinyu","has default category:" +
+		// intent.hasCategory(Intent.DEFAULT_CATEGORY));
 		startActivity(intent);
 	}
 
-
-
-	private final static int ADD_MEMO_ID = 1;
+	private final static int ADD_MEMO_ITEM = 1;
+	private final static int DELETE_MEMO_ITEM = 2;
 
 	private Cursor cursor;
-
 
 	@Override
 	protected void onDestroy() {
@@ -142,8 +152,5 @@ public class MemoList extends ListActivity {
 		Log.d(TAG, "onFreeze");
 		super.onFreeze(outState);
 	}
-
-	
-	
 
 }
